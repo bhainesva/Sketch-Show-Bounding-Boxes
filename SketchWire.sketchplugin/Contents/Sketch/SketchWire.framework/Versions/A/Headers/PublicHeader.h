da80533028539386cfe541ab19f07b67726ba0f5
pravdomil <pravdomil.toman@gmail.com>
2018-01-16 18:57:59 +0100

    SketchWire.framework

---
+++ SketchWire.sketchplugin/Contents/Sketch/SketchWire.framework/Versions/A/Headers/PublicHeader.h
@@ -0,0 +1,19 @@
+//
+//  SketchWire.h
+//  SketchWire
+//
+//  Created by Pravdomil Toman on 15/01/2018.
+//  Copyright © 2018 Pravdomil Toman. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SketchWire.
+FOUNDATION_EXPORT double SketchWireVersionNumber;
+
+//! Project version string for SketchWire.
+FOUNDATION_EXPORT const unsigned char SketchWireVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SketchWire/PublicHeader.h>
+
+
