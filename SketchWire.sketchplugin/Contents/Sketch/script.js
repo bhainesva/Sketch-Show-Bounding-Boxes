6bfef324fc91aa25ae238ed0456b6977860f49d1
pravdomil <pravdomil.toman@gmail.com>
2018-01-16 18:57:02 +0100

    script

---
+++ SketchWire.sketchplugin/Contents/Sketch/script.js
@@ -0,0 +1,38 @@
+const name = "SketchWire";
+
+function onStart(context) {
+  const scriptPath = context.scriptPath;
+  const directory = [scriptPath stringByDeletingLastPathComponent];
+  
+  const loaded = [[Mocha sharedRuntime] loadFrameworkWithName:name inDirectory:directory];
+  if (!loaded) {
+    log(name + " loadFrameworkWithName failed");
+    return;
+  }
+  
+  const frameworkClass = NSClassFromString(name);
+  if (!frameworkClass) {
+    log(name + " NSClassFromString failed");
+    return;
+  }
+  
+  if (![frameworkClass install]) {
+    log(name + " install failed");
+    return;
+  }
+  
+  log(name + " OK");
+}
+
+function toggleSketchWire(context) {
+  const frameworkClass = NSClassFromString(name);
+  if (![frameworkClass toggle]) {
+    log(name + " toggle failed");
+    return;
+  }
+}
+
+function donateSketchWire(context) {
+    const url = "https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=BCL2X3AFQBAP2&item_name=Sketch%20Wire%20Beer";
+    NSWorkspace.sharedWorkspace().openURL(NSURL.URLWithString(url));
+}
