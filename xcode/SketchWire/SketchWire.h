137cf27f97065e4b13e2235b3e8da96225d77c5c
pravdomil <pravdomil.toman@gmail.com>
2018-01-20 21:41:17 +0100

    MSImmutableLayer inherits from NSObject

--- xcode/SketchWire/SketchWire.h
+++ xcode/SketchWire/SketchWire.h
@@ -13,7 +13,7 @@
 - (void)renderLayerUncached:(id)arg1 ignoreDrawingArea:(BOOL)arg2 context:(id)arg3;
 @end
 
-@interface MSImmutableLayer
+@interface MSImmutableLayer : NSObject
 @property(readonly, nonatomic) struct CGRect rect;
 @end
 
