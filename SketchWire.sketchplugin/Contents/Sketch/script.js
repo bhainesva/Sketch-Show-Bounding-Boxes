58d8f0928df7998e41ce5e6d97f732ff91d4fabf
pravdomil <pravdomil.toman@gmail.com>
2018-01-20 22:12:49 +0100

    toggleCrossSketchWire

--- SketchWire.sketchplugin/Contents/Sketch/script.js
+++ SketchWire.sketchplugin/Contents/Sketch/script.js
@@ -29,6 +29,11 @@ function toggleSketchWire(context) {
   [frameworkClass toggle];
 }
 
+function toggleCrossSketchWire(context) {
+  const frameworkClass = NSClassFromString(name);
+  [frameworkClass toggleCross];
+}
+
 function donateSketchWire(context) {
     const url = "https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=BCL2X3AFQBAP2&item_name=Sketch%20Wire%20Beer";
     NSWorkspace.sharedWorkspace().openURL(NSURL.URLWithString(url));
