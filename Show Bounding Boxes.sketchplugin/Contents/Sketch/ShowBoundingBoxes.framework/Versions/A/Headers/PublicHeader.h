e8f8d103d74be53634a44b77747d6ea05822b9cd
pravdomil <pravdomil.toman@gmail.com>
2018-01-20 23:06:23 +0100

    build

--- Show Bounding Boxes.sketchplugin/Contents/Sketch/ShowBoundingBoxes.framework/Versions/A/Headers/PublicHeader.h
+++ Show Bounding Boxes.sketchplugin/Contents/Sketch/ShowBoundingBoxes.framework/Versions/A/Headers/PublicHeader.h
@@ -1,11 +1,11 @@
 #import <Cocoa/Cocoa.h>
 
-//! Project version number for SketchWire.
-FOUNDATION_EXPORT double SketchWireVersionNumber;
+//! Project version number for ShowBoundingBoxes.
+FOUNDATION_EXPORT double ShowBoundingBoxesVersionNumber;
 
-//! Project version string for SketchWire.
-FOUNDATION_EXPORT const unsigned char SketchWireVersionString[];
+//! Project version string for ShowBoundingBoxes.
+FOUNDATION_EXPORT const unsigned char ShowBoundingBoxesVersionString[];
 
-// In this header, you should import all the public headers of your framework using statements like #import <SketchWire/PublicHeader.h>
+// In this header, you should import all the public headers of your framework using statements like #import <ShowBoundingBoxes/PublicHeader.h>
 
 
